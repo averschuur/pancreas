6c7bfef78b01826ee6bf17568f398160e1f1e14f
Christoph Geisenberger <chrgeisenberger@gmx.de>
2023-03-06 17:41:10 +0100

    Added code to produce heatmaps for cell type deconvolution

--- scripts/5_celltype_deconvolution.R
+++ scripts/5_celltype_deconvolution.R
@@ -24,75 +24,46 @@ source("./scripts/0_helpers.R")
 # load annotation
 anno <- readRDS(file = "./output/sample_annotation_umap_purity.rds")
 
-# load data for cel type deconvolution
+# add outlier detection results
+od <- readRDS(file = "./output/outlier_det_results.rds")
+od <- od %>% 
+  select(-tumorType)
+anno <- left_join(anno, od, by = "arrayId")
+rm(od)
+
+# load data for cell type deconvolution
 decomp <- read_csv("./test/meth_atlas/pancreas_data_deconv_output.csv")
+decomp_rownames <- decomp$cell_type
+decomp <- as.matrix(decomp[, -1])
+rownames(decomp) <- decomp_rownames
+rm(decomp_rownames)
+decomp <- decomp[, anno$arrayId]
 
-decomp_mat <- as.matrix(decomp[, -1])
-rownames(decomp_mat) <- decomp$cell_type
-
-
-decomp_umap <- umap(t(decomp_mat))
-
-
-
-
-clusters_n <- 7
-km <- kmeans(x = cor(decomp_mat), centers = clusters_n)
-hc <- (1 - cor(decomp_mat)) %>% 
-  as.dist %>% 
-  hclust %>% 
-  cutree(k = clusters_n)
-
-decomp_anno <- tibble(arrayId = colnames(decomp_mat), 
-                      umap_decomp_x = decomp_umap$layout[, 1], 
-                      umap_decomp_y = decomp_umap$layout[, 2], 
-                      clusters_km = km$cluster, 
-                      clusters_hc = hc)
-
-anno <- readRDS(file = "./output/sample_annotation_umap_purity.rds")
-
-anno <- left_join(anno, decomp_anno)
-
-# tumor type
+# first, look at misclassification rate per tumor type
+anno <- anno %>% 
+  mutate(class_correct = ifelse(winning_class == tumorType, 1, 0))
 anno %>% 
-  ggplot(aes(umap_x, umap_y, col = tumorType)) +
-  geom_point(size = 4)
-
-# kmeans
-anno %>% 
-  ggplot(aes(umap_x, umap_y, col = as.factor(clusters_km))) +
-  geom_point(size = 4)
-
-# hc
-anno %>% 
-  ggplot(aes(umap_x, umap_y, col = as.factor(clusters_hc))) +
-  geom_point(size = 4)
+  group_by(tumorType) %>% 
+  summarise(no_correct = sum(class_correct == 0), 
+            prop_correct = sum(class_correct)/n())
 
 
-decomp <- decomp %>% 
-  pivot_longer(cols = 2:437, names_to = "arrayId", values_to = "prop")
-decomp <- decomp %>% 
-  select(arrayId, cell_type, prop)
-colnames(decomp) <- decomp[, 1]
+# look at data for specific tumor types
+anno_sub <- anno %>% 
+  filter(tumorType == "PanNET")
 
-anno <- left_join(anno, decomp, multiple = "all")
+anno_sub_df <- anno_sub %>% 
+  select(estimate, class_correct, od_class) %>% 
+  mutate(class_correct = as.factor(class_correct)) %>% 
+  as.data.frame
+rownames(anno_sub_df) <- anno_sub$arrayId
+
+decomp_sub <- decomp[, anno_sub$arrayId]
+
+pheatmap::pheatmap(decomp_sub, annotation_col = anno_sub_df)
+    
 
 
-test <- anno %>% 
-  #group_by(tumorType, cell_type) %>% 
-  #summarise(mean_prop = mean(prop)) %>% 
-  filter(tumorType == "PanNET") %>% 
-  select(arrayId, cell_type, prop) %>% 
-  pivot_wider(names_from = cell_type, values_from = prop) %>% 
-  select(-1) %>% 
-  as.matrix() %>% 
-  pheatmap::pheatmap()
-ggplot(aes(arrayId, cell_type, size = prop, col = prop)) +
-  geom_point()
-theme(legend.position = "none") +
-  facet_wrap(facets = vars(tumorType))
-ungroup() %>% 
-  select(2:26) %>% 
-  pheatmap::pheatmap(cluster_rows = FALSE)
-ggplot(aes(avg_beta, prop, col = cell_type)) +
-  geom_col()
+
+
+
