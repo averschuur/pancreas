1b4d6934ceeb6edb06ef000f568b21c5013e635d
Christoph Geisenberger <chrgeisenberger@gmx.de>
2023-05-25 22:42:11 +0200

    Added analysis for mixed tumors

--- scripts/6_mixed tumors_unsupervised_analysis.R
+++ scripts/6_mixed tumors_unsupervised_analysis.R
@@ -15,131 +15,116 @@ library(caret)
 source("./scripts/0_helpers.R")
 
 
-### Prepare data ----------------------------------------------------------------
-# load sample betas and annotation and filter 
 
-anno <- readRDS("./output/sample_annotation_umap_purity.rds")
+### Prepare data ----------------------------------------------------------------
+
+
+# load anno, RF model and methylation data
+anno <- readRDS("./input/sample_annotation.rds")
+rf_model <- readRDS(file = "./output/rf_model_default.rds")
 betas <- readRDS(file = "./input/betas_pancreas_everything.rds")
 
-betas <- betas[, anno$arrayId]
+# include mixed tumors
+anno <- anno %>% 
+  mutate(subtype = tumorType, 
+         tumorType = str_replace(tumorType, "Mixed.*", "Mixed"), 
+         mixed = as.factor(ifelse(tumorType == "Mixed", 1, 0))) %>% 
+  filter(tumorType %in% c("PB", "ACC", "PanNEC", "PanNET", "NORM", "PDAC", "SPN", "Mixed")) %>% 
+  filter(location %in% c("primary", "pancreas"))
 
-# obtain model performance 
-rf_model <- readRDS(file = "./output/rf_model_default.rds")
+# filter betas
+betas <- betas[, anno$arrayId]
+betas_model <- betas[names(rf_model$trainingData)[1:5000], ]
 
 # rf predictions
 rf_pred_class <- predict(rf_model, newdata = t(betas))
-rf_pred_scores <- predict(rf_model, newdata = t(betas), type = "prob")
+rf_pred_scores <- predict(rf_model, newdata = t(betas), type = "prob") %>% 
+  as_tibble()
+rf_pred_scores <- rename_with(rf_pred_scores, ~ paste0("scores_", .x))
 rf_pred_scores_max <- apply(rf_pred_scores, 1, max)
-rf_pred_class <- apply(rf_pred_scores, 1, function(x) colnames(rf_pred_scores)[which.max(x)]) %>% as.factor
 
+# rf score entropy
+rf_entropy <- apply(rf_pred_scores, 1, entropy)
+beta_var <- apply(betas_model, 2, var)
+beta_entropy <- apply(betas_model, 2, entropy)
 
-# add performance to annotation
+# purity estimates
+estimate <- RFpurify::predict_purity_betas(betas = betas, method = "ESTIMATE")
+absolute <- RFpurify::predict_purity_betas(betas = betas, method = "ABSOLUTE")
+
+# add to annotation
 anno <- anno %>% 
-  mutate(pred_rf = rf_pred_class, 
-         pred_scores_rf = apply(rf_pred_scores, 1, max))
+  mutate(estimate = estimate, 
+         absolute = absolute, 
+         rf_class = rf_pred_class, 
+         rf_winning_score = rf_pred_scores_max, 
+         rf_entropy = rf_entropy,
+         beta_entropy = beta_entropy, 
+         beta_var = beta_var)
 
+anno <- bind_cols(anno, rf_pred_scores)
 
-# (1) Calculate the correlations for the RF output matrix for all samples ----------------------------------------
+# check correlations between variables
 anno %>% 
-  ggplot(aes(pred_rf, pred_scores_rf)) +
-  geom_boxplot()
+  select(tumorType, mixed, absolute, estimate, 
+         starts_with("rf"), starts_with("beta")) %>% 
+  GGally::ggpairs()
 
-cor(rf_pred_scores)
-cor(rf_pred_scores$ACC, rf_pred_scores$PB)
+ggsave(filename = "./plots/mixed_tumors_pairwise_plots.pdf")
 
+# heatmap of sample-wise correlations
+sample_cor <- cor(betas_model)
+betas_cor <- cor(t(betas_model))
 
+rownames(sample_cor) <- ifelse(anno$tumorType == "Mixed", "Mixed", ".")
+#pdf(file = "plots/mixed_tumors_sample_correlation_heatmap.pdf")
+pheatmap::pheatmap(sample_cor, show_colnames = FALSE)
+#dev.off()
 
-# (2) Calculate the correlations for the RF output matrix for each class -----------------------------------------
-names <- rownames(rf_pred_scores)
-rownames(rf_pred_scores) <- NULL
-data <- cbind(names,rf_pred_scores)
-colnames(data) <- c("arrayId", "ACC", "NORM", "PanNEC", "PanNET", "PB", "PDAC", "SPN")
-
-# take only ACC samples
-ACC <- anno %>%
-  filter(tumorType == "ACC") %>%
-  select("arrayId")
-
-ACC <- left_join(ACC, data, by = "arrayId")
-ACC <- column_to_rownames(ACC, var = "arrayId")
-cor(ACC)
+# run UMAP umap
+set.seed(12341234)
+umap <- umap(d = sample_cor)
 
 
-# take all PB samples
-PB <- anno %>%
-  filter(tumorType == "PB") %>%
-  select("arrayId")
 
-PB <- left_join(PB, data, by = "arrayId")
-PB <- column_to_rownames(PB, var = "arrayId")
-cor(PB)
+# look into detail regarding mixed tumors --------------------------------------
 
-# take all SPN samples
-SPN <- anno %>%
-  filter(tumorType == "SPN") %>%
-  select("arrayId")
+# RF score entropy
+anno %>% 
+  ggplot(aes(mixed, rf_entropy, fill = mixed)) +
+  geom_boxplot() +
+  theme_classic(base_size = 18) +
+  labs(x = NULL, y = "RF score entropy")
 
-SPN <- left_join(SPN, data, by = "arrayId")
-SPN <- column_to_rownames(SPN, var = "arrayId")
-cor(SPN)
-
-# take all PanNET samples
-PNET <- anno %>%
-  filter(tumorType == "PanNET") %>%
-  select("arrayId")
-
-PNET <- left_join(PNET, data, by = "arrayId")
-PNET <- column_to_rownames(PNET, var = "arrayId")
-cor(PNET)
-
-# take all PanNEC samples
-PNEC <- anno %>%
-  filter(tumorType == "PanNEC") %>%
-  select("arrayId")
-
-PNEC <- left_join(PNEC, data, by = "arrayId")
-PNEC <- column_to_rownames(PNEC, var = "arrayId")
-cor(PNEC)
-
-# take all NORM samples
-NORM <- anno %>%
-  filter(tumorType == "NORM") %>%
-  select("arrayId")
-
-NORM <- left_join(NORM, data, by = "arrayId")
-NORM <- column_to_rownames(NORM, var = "arrayId")
-cor(NORM)
+anno %>% 
+  ggplot(aes(mixed, estimate, fill = mixed)) +
+  geom_boxplot() +
+  theme_bw(base_size = 18) +
+  labs(x = NULL, y = "Tumor Purity")
 
 
-# (3) Calculate the correlations for the RF output matrix for the mixed tumors -----------------------------------
-anno <- readRDS("./input/sample_annotation.rds")
-
-# select Mixed tumors 
-annoM <- anno %>%
-  filter(tumorType %in% c("Mixed", "Mixed_ACC_DA", "Mixed_ACC_NEC", "Mixed_ACC_DA_NEC"))
-
-# load unfiltered beta values
-betas <- readRDS("./input/betas_pancreas_everything.rds")
-betasM <- betas[, annoM$arrayId]
-
-# obtain model performance 
-rf_model <- readRDS(file = "./output/rf_model_default.rds")
-
-# rf predictions
-rf_pred_class <- predict(rf_model, newdata = t(betasM))
-rf_pred_scores <- predict(rf_model, newdata = t(betasM), type = "prob")
-rf_pred_scores_max <- apply(rf_pred_scores, 1, max)
-rf_pred_class <- apply(rf_pred_scores, 1, function(x) colnames(rf_pred_scores)[which.max(x)]) %>% as.factor
+anno %>% 
+  select(tumorType, starts_with("scores")) %>% 
+  pivot_longer(cols = starts_with("scores")) %>% 
+  mutate(name = str_replace_all(name, "scores_", "")) %>% 
+  mutate(name = str_replace_all(name, "Pan", "")) %>% 
+  group_by(tumorType, name) %>% 
+  summarise(mean_score = mean(value)) %>% 
+  ungroup %>% 
+  ggplot(aes(name, mean_score, fill = name)) +
+  geom_col() +
+  theme_bw() +
+  labs(y = "Random Forest Score (mean)", fill = "Tumor Type") +
+  facet_wrap(facets = vars(tumorType))
 
 
-# add performance to annotation
-annoM <- annoM %>% 
-  mutate(pred_rf = rf_pred_class, 
-         pred_scores_rf = apply(rf_pred_scores, 1, max))
+anno %>% 
+  filter(tumorType %in% c("PB", "ACC", "Mixed")) %>% 
+  ggplot(aes(scores_ACC, scores_PB)) +
+  geom_point(aes(size = mixed, col = tumorType)) +
+  theme_bw(base_size = 18) +
+  labs(x = "Score ACC", y = "Score PB")
 
-# get correlation
-cor(rf_pred_scores)
 
-# (3) calculate entropy ------------------------------------------------------------------------------------------
--sum(rf_pred_scores * log2(rf_pred_scores))
+browseVignettes("EpiSCORE")
 
