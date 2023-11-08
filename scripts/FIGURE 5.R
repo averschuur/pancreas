aa034ab0aa82704c46825de5f2fa71a8e0e85359
Anna Vera Verschuur <annaveraverschuur@gmail.com>
2023-11-08 22:15:58 +0100

    folder clean-up and FIGURE 5 clean-up

--- scripts/FIGURE 5.R
+++ scripts/FIGURE 5.R
@@ -7,46 +7,46 @@ library(minfi)
 library(doParallel)
 
 ### prepare data
-idats <- list.files(path = "./data/UMCU/Idat Files_All/",
+#idats <- list.files(path = "./data/UMCU/Idat Files_All/",
                     recursive = TRUE,
                     full.names = TRUE, 
                     pattern = "_Grn.idat")
 
-raw <- read.metharray(basenames = idats, force = TRUE)
+#raw <- read.metharray(basenames = idats, force = TRUE)
 
 # preprocess data
-preprocessed_epic <- raw %>%
-  preprocessNoob(dyeMethod= "single")
+#preprocessed_epic <- raw %>%
+#  preprocessNoob(dyeMethod= "single")
 
-betas <- getBeta(preprocessed_epic)
+#betas <- getBeta(preprocessed_epic)
 
 # save unfiltered betas
-saveRDS(object = betas, file = "./input/betas_UMCU_unfiltered.rds")
-betas_UMCU <- readRDS("./input/betas_UMCU_unfiltered.rds")
+#saveRDS(object = betas, file = "./input/betas_UMCU_unfiltered.rds")
+betas_UMC <- readRDS("./input/betas_UMCU_unfiltered.rds")
 
 # create annotation
-anno_files <- list.files(path = "./annotation/",
-                         pattern = ".csv",
-                         full.names = TRUE)
-anno_files <- anno_files[grepl(x = anno_files, pattern = "*annotation_umcu")]
-anno_files <- anno_files[!grepl(x = anno_files, pattern = "*annotation_umcu_paired_samples")]
-anno_UMC <- lapply(as.list(anno_files), read_csv)
-anno_UMC <- Reduce(f = bind_rows, x = anno_UMC)
+#anno_files <- list.files(path = "./annotation/",
+#                         pattern = ".csv",
+#                         full.names = TRUE)
+#anno_files <- anno_files[grepl(x = anno_files, pattern = "*annotation_umcu")]
+#anno_files <- anno_files[!grepl(x = anno_files, pattern = "*annotation_umcu_paired_samples")]
+#anno_UMC <- lapply(as.list(anno_files), read_csv)
+#anno_UMC <- Reduce(f = bind_rows, x = anno_UMC)
 
 
-saveRDS(object = anno, file = "./output/anno_UMCU.rds")
+#saveRDS(object = anno_UMC, file = "./output/anno_UMCU.rds")
 
 # open data
-anno <- readRDS("./output/sample_annotation_umap_purity.rds")
-betas <- readRDS(file = "./output/betas_UMCU_unfiltered.rds")
+anno_UMC <- readRDS("./output/sample_annotation_umap_purity.rds")
+betas_UMC <- readRDS(file = "./input/betas_UMCU_unfiltered.rds")
 
 rf_model <- readRDS(file = "./output/rf_model_default.rds")
 xgb_model <- readRDS(file = "./output/xgb_model_default.rds")
 nn_model <- load_model_hdf5(file = "./output/nn_model.hdf5")
 
 # rf predictions
-rf_pred_class <- predict(rf_model, newdata = t(betas))
-rf_pred_scores <- predict(rf_model, newdata = t(betas), type = "prob")
+rf_pred_class <- predict(rf_model, newdata = t(betas_UMC))
+rf_pred_scores <- predict(rf_model, newdata = t(betas_UMC), type = "prob")
 rf_pred_scores_max <- apply(rf_pred_scores, 1, max)
 rf_pred_class <- apply(rf_pred_scores, 1, function(x) colnames(rf_pred_scores)[which.max(x)]) %>% as.factor
 
@@ -80,7 +80,7 @@ UMCU_SPN1 %>%
         axis.line = element_blank()) +
   labs(x = NULL, y = NULL) +
   coord_polar()
-ggsave("Figure 5_UMCU_SPN1.pdf", path= "./plots/", dpi=500)
+ggsave("Figure 5_UMCU_SPN1_08112023.pdf", path= "./plots/", dpi=500)
 
 ### Extract data for UMCU_ACC1 = 205555390010_R07C01
 UMCU_ACC1 <- test[test$arrayId == "205555390010_R07C01",]
@@ -100,7 +100,7 @@ UMCU_ACC1 %>%
         axis.line = element_blank()) +
   labs(x = NULL, y = NULL) +
   coord_polar()
-ggsave("Figure 5_UMCU_ACC1.pdf", path= "./plots/", dpi=500)
+ggsave("Figure 5_UMCU_ACC1_08112023.pdf", path= "./plots/", dpi=500)
 
 
 ### Extract data for UMCU_ACC1_m = 205555390025_R01C01
@@ -121,7 +121,7 @@ UMCU_ACC1_m %>%
         axis.line = element_blank()) +
   labs(x = NULL, y = NULL) +
   coord_polar()
-ggsave("Figure 5_UMCU_ACC1_m.pdf", path= "./plots/", dpi=500)
+ggsave("Figure 5_UMCU_ACC1_m_08112023.pdf", path= "./plots/", dpi=500)
 
 ### Extract data for UMCU_ACC2 = 206601450125_R05C01
 UMCU_ACC2 <- test[test$arrayId == "206601450125_R05C01",]
@@ -141,25 +141,4 @@ UMCU_ACC2 %>%
         axis.line = element_blank()) +
   labs(x = NULL, y = NULL) +
   coord_polar()
-ggsave("Figure 5_UMCU_ACC2.pdf", path= "./plots/", dpi=500)
-
-
-### Extract data for UMCU_ACC3 = 206601450016_R01C01
-UMCU_ACC3 <- test[test$arrayId == "206601450016_R01C01",]
-
-UMCU_ACC3 %>%
-  ggplot(aes(tumor_type, rf_score, fill = tumor_type)) +
-  geom_col() +
-  theme_bw(base_size = 18) +
-  paletteer::scale_fill_paletteer_d("rcartocolor::Safe") +
-  theme(panel.border = element_blank(),
-        panel.grid = element_line(color = "grey",
-                                  size = 0.5),
-        legend.position = "none",
-        axis.ticks = element_blank(),
-        axis.text = element_text(color = "black"),
-        axis.title = element_blank(),
-        axis.line = element_blank()) +
-  labs(x = NULL, y = NULL) +
-  coord_polar()
-ggsave("Figure 5_UMCU_ACC3.pdf", path= "./plots/", dpi=500)
+ggsave("Figure 5_UMCU_ACC2_08112023.pdf", path= "./plots/", dpi=500)
